064d2ac899a9a26be18835bd4f4345cd83259272
neyckeraldrich <neyckeraldrich@outlook.com>
2021-12-28 11:50:49 -0500

    update

--- install-ubuntu-docker.sh
+++ install-ubuntu-docker.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
 
-sudo docker run hello-world
+docker run hello-world
 
 # Linux post-install
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+groupadd docker
+usermod -aG docker $USER
+systemctl enable docker
